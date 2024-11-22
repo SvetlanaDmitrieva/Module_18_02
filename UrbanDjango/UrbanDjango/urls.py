67aafafb9dc3a195e547f7cc4a905bd3e23cf785
SvetlanaDmitrieva <sv-dmitrieva@list.ru>
2024-11-22 14:55:48 +0300

    22/11-02

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from task2.views import class_temp, func_temp
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', func_temp),
+    path('index/', class_temp.as_view()),
 ]
